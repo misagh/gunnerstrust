6f215e150558471e3fafe4a9defcdcd8501ea6f3
Misagh <misagh1663@gmail.com>
2020-04-10 15:59:10 +0100

    Add shadow to user avatar in list

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -8,7 +8,7 @@
         @foreach($gunners as $gunner)
             <div class="col mb-4">
                 <div class="card text-center shadow">
-                    <img src="{{ $gunner->avatar }}" class="card-img-top">
+                    <img src="{{ $gunner->avatar }}" class="card-img-top shadow-sm">
                     <div class="card-body">
                         <h5 class="card-title eng-font font-weight-bold mb-3"><a class="stretched-link" href="{{ route('users.profile', $gunner->username) }}">{{ $gunner->username }}</a></h5>
                         <small class="card-text text-white py-1 px-3 rounded username-{{ $gunner->role }}">{{ $gunner->title }}</small>
