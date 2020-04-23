2f1f1eaa0008f36c6caee9dd8684eb2793e9cd52
Misagh <misagh1663@gmail.com>
2020-04-23 14:43:37 +0100

    Add google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="fa">
 <head>
+    @include('layouts.ga')
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
