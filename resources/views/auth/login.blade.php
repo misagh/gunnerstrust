e950a61d621cf9d4772e36263f584e80b2824635
Misagh <misagh1663@yahoo.com>
2020-07-05 10:33:53 +0100

    Default login remember checked

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -65,7 +65,7 @@
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" checked>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('مرا به خاطر داشته باش') }}
