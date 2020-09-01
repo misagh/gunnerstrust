b65f3b583e0ec9e9548d7b727cd92d3d96be9386
Misagh <misagh1663@yahoo.com>
2020-09-01 12:08:43 +0100

    Fix auth id bug in articles

--- resources/views/articles/view.blade.php
+++ resources/views/articles/view.blade.php
@@ -53,7 +53,7 @@
                                 <a target="_blank" href="{{ route('articles.short', base64url_encode($article->id)) }}" class="rounded text-white bg-secondary py-1 px-3" rel="nofollow">لینک کوتاه</a>
                                 <a target="_blank" href="{{ $article->source }}" class="rounded text-white bg-info py-1 px-3">لینک منبع</a>
                             </div>
-                            @if (is_admin($auth) || $auth->id === $article->user_id)
+                            @if (is_admin($auth) || ($auth->id ?? null) === $article->user_id)
                             <div class="float-right small mt-5">
                                 <a href="{{ route('articles.edit', $article->id) }}" class="rounded bg-warning py-1 px-3">ویرایش خبر</a>
                             </div>
